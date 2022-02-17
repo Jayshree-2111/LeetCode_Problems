bf347c1379b29e200486c25b64ecf17bd6d451cb
Jayshree-2111 <86256037+Jayshree-2111@users.noreply.github.com>
2022-02-18 00:03:51 +0530

    Create FizzBuzz.cpp

---
+++ Top Interview Questions_MATHS/FizzBuzz.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+
+        for(int i=1;i<=n;i++)
+        {
+        
+          //APPROACH-1
+           /* if(i%3==0)
+            {
+                if(i%5==0)
+                {
+                    result.push_back("FizzBuzz");
+                }
+                else
+                {
+                    result.push_back("Fizz");
+                }
+            }
+            else if(i%5==0)
+            {
+                result.push_back("Buzz");
+            }*/
+            
+            //APPROACH-2 (More Optimised)
+            if(i%3==0 && i%5==0)
+                result.push_back("FizzBuzz");
+            else if (i%3==0)
+                result.push_back("Fizz");
+            else if (i%5==0)
+                result.push_back("Buzz");
+            else
+                 result.push_back(to_string(i));
+       
+        }
+        return result;
+    }
+};
